8d0a7f97982425a23ff6e7938786acc9c6b046ec
NooBit0 <58011434+NooBit0@users.noreply.github.com>
2019-11-25 23:06:07 +0300

    Add files via upload
    Adding testing commentary

--- lab2/Circle.cs
+++ lab2/Circle.cs
@@ -14,6 +14,7 @@ namespace CircleNameSpace
         #region Constructor
         public Circle(int numberParties, int circleCenterX, int circleCenterY, double radius)
         {
+            //testing comment
             _point = new Points(circleCenterX, circleCenterY);
             _polygon = new Polygon();
             Radius = radius;
